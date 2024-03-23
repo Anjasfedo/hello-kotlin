3c35486a1a90a785854cba4219f03d43a3ea8c13
AnjasFedo <fedoaf98@gmail.com>
2024-03-23 21:40:22 +0700

    Collection: List

--- src/Main.kt
+++ src/Main.kt
@@ -3,18 +3,7 @@
 fun saySomething(greeting: String, something: String) = println("$greeting $something")
 
 fun main() {
-    val interestingThings = arrayOf("Kotlin", "Programming", "Manhua")
+    val interestingThings = listOf("Kotlin", "Programming", "Manhua")
 
-    println(interestingThings.size)
-
-    println(interestingThings[0])
-    println(interestingThings.get(0))
-
-//    for (interestingThing in interestingThings) {
-//        println(interestingThing)
-//    }
-
-//    interestingThings.forEach { interestingThing -> println(interestingThing) }
-
-    interestingThings.forEachIndexed { index, interestingThing -> println("$interestingThing at index $index") }
+    interestingThings.forEach { interestingThing -> println(interestingThing) }
 }
