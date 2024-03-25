f6f8596f6020e78f28b1359499da08a6d91e241b
AnjasFedo <fedoaf98@gmail.com>
2024-03-25 15:56:24 +0700

    Overriding Methods

--- src/PersonInfoProvider.kt
+++ src/PersonInfoProvider.kt
@@ -2,9 +2,16 @@ interface PersonInfoProvider {
     fun printInfo(person: Person)
 }
 
-abstract class BasicInfoProvider : PersonInfoProvider
+class BasicInfoProvider : PersonInfoProvider {
+    override fun printInfo(person: Person) {
+        println("BasicInfoProvider")
+        person.printInfo()
+    }
+}
 
 fun main() {
-    val provider = BasicInfoProvider
+    val provider = BasicInfoProvider()
+
+    provider.printInfo(Person())
 }
 
