9d2d24479a91aa3cc80964d617e9c31d21485b57
AnjasFedo <fedoaf98@gmail.com>
2024-03-25 15:38:52 +0700

    Custom Getters/Setters

--- src/Person.kt
+++ src/Person.kt
@@ -1,14 +1,14 @@
 class Person(val firstName: String = "Peter", val lastName: String = "Parker") {
-
-//    init {
-//        println("init 1")
-//    }
-//
-//    constructor(): this(firstName = "Peter", lastName = "Parker") {
-//        println("secondary constructor")
-//    }
-//
-//    init {
-//        println("init 2")
-//    }
+//    by default compile
+//    var have getter & setter
+//    val only getter
+    var userName: String? = null
+        set(value) {
+            field = value
+            println("the new username is $value")
+        }
+        get() {
+            println("the returned value is $field")
+            return field
+        }
 }
